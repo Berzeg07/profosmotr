64a6483b5bc9f9418040711091010757da4569e6
Aslan <berezg86@gmail.com>
2020-11-03 15:15:08 +0300

    Верстка всех блоков

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -5,4 +5,28 @@ $(document).ready(function () {
     });
 
     $('.custom-select select').selectric();
+
+    var swiper = new Swiper('.blockinfo__gallery', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    $(function () {
+        if (window.matchMedia('(min-width: 1280px)').matches) {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() >= 140) {
+                    $('.headerdes-sticky').addClass('is-active');
+                }
+                else {
+                    $('.headerdes-sticky').removeClass('is-active');
+                }
+            });
+        }
+    });
 });
